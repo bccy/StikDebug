76b0daf92f60279197922fadb7a01ddddbf81170
远兮 <bccy@users.noreply.github.com>
2026-08-16 03:16:25 +0800

    搜索tab改为纯图标(无文字)

--- StikJIT/Views/MainTabView.swift
+++ StikJIT/Views/MainTabView.swift
@@ -45,7 +45,7 @@ struct MainTabView: View {
                 .tag(Self.actionBookmarksTag)
 
             Color.clear
-                .tabItem { Label("搜索", systemImage: "magnifyingglass") }
+                .tabItem { Image(systemName: "magnifyingglass") }
                 .tag(Self.actionSearchTag)
 
             SettingsView()
